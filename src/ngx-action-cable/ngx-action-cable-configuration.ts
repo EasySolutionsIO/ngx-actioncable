c9b90767853cb056ddeb86d2ca12282cc73aebb1
Robert Wurm <robert.wurm@easysol.at>
2019-10-21 09:46:08 +0200

    ADD multiple websockets

--- src/ngx-action-cable/ngx-action-cable-configuration.ts
+++ src/ngx-action-cable/ngx-action-cable-configuration.ts
@@ -1,3 +1,16 @@
 export class NgxActionCableConfiguration {
-    constructor(public url: string) {}
+    
+    urls = new Map<string, string>();
+
+    constructor(url: string, key?: string) {
+        if (key == null) {
+            key = ''; // default key
+        }
+        this.urls.set(key, url);
+    }
+    
+    addUrl(key: string, url: string) {
+        this.urls.set(key, url);
+    }
+
 }
